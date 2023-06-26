031410d2a7928826f432e87a8f352e4b5b800bf4
sumit-hanumante <sumithanumanteblog@gmail.com>
2023-06-26 15:30:15 +0530

    initial commit

---
+++ src/main/java/com/example/EasyContentManager/EasyContentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EasyContentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyContentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyContentManagerApplication.class, args);
+	}
+
+}
